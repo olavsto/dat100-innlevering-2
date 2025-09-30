c195a0f49cccf11e282c1cfa90622c5472ffd5ce
VegardVold <127211323+VegardVold@users.noreply.github.com>
2025-09-30 14:34:25 +0200

    Update Tabeller.java

--- src/main/java/no/hvl/dat100/tabeller/Tabeller.java
+++ src/main/java/no/hvl/dat100/tabeller/Tabeller.java
@@ -5,28 +5,24 @@ public class Tabeller {
 	// a)
 	public static void skrivUt(int[] tabell) {
 
-<<<<<<< HEAD
         for (int i = 0; i < tabell.length; i++) {
             System.out.println(" " + tabell[i]);
         }
         System.out.println();
-=======
-		//int[] tabell = {1, 2, 3, 4, 5};
-
-		throw new UnsupportedOperationException("Metoden skrivUt ikke implementert");
-
->>>>>>> 57d269381a573d44df99f95c2abb1445bf313f53
 	}
 
 	// b)
 	public static String tilStreng(int[] tabell) {
 
-       // String tabell = tabell;
-       // int[] tabell = {42, 67, 89};
-
-        System.out.println();
-
-		throw new UnsupportedOperationException("Metoden tilStreng ikke implementert");
+        String streng = "[";
+        for (int i = 0; i < tabell.length; i++) {
+            streng += tabell[i];
+            if (i < tabell.length - 1) {
+                streng += ",";
+            }
+        }
+        streng += "]";
+        return streng;
 	}
 
 	// c)
